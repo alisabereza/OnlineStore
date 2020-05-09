8ac1715b72e8d882e1991affe8842c15bfde8ee1
alisabereza <alisabereza@gmail.com>
2020-05-09 15:35:36 +0300

    added Equals and Hashcode to user

--- src/main/java/com/berezovska/store/model/User.java
+++ src/main/java/com/berezovska/store/model/User.java
@@ -1,32 +1,51 @@
 package com.berezovska.store.model;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
 
-@EqualsAndHashCode(callSuper = true)
+
 @Entity
 @NoArgsConstructor
 @Table (name = "users")
 public @Data class User extends BaseEntity {
-
+    @NotEmpty
     @Column (name = "password")
     private String password;
+    @NotEmpty
     @Email
     @Column (name = "email")
     private String email;
+    @NotEmpty
     @Column (name = "first_name")
     private String firstName;
+    @NotEmpty
     @Column (name = "last_name")
     private String lastName;
+
     @Column (name = "user_role")
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
     @Column (name = "status")
     @Enumerated(EnumType.STRING)
     private UserStatus status;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return firstName.equals(user.firstName) &&
+                lastName.equals(user.lastName) &&
+                email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, email);
+    }
 
 }
